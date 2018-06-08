d1dbb98fbc3d2acc5cc1a0c459efebf45aa1e200
weiwei1988 <zhaoweiheng8828@gmail.com>
2018-06-09 01:39:29 +0900

    Change test

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 print(np.power(2, 2))
+
 print(np.array([1, 2, 3, 4, 5]))
+a = np.linspace(0, 1, 10)
+df = pd.DataFrame(a)
+df
