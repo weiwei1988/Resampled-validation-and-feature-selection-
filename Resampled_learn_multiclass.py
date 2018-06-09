70541ec1f39d1f9bda0b3c6eb8545c576f9c74ec
weiwei1988 <zhaoweiheng8828@gmail.com>
2018-06-10 04:44:37 +0900

    Minor Change

--- Resampled_learn_multiclass.py
+++ Resampled_learn_multiclass.py
@@ -545,7 +545,7 @@ class Resampled_RFE:
                 ACC, ROC_AUC, F1, PRE, REC, logloss, IM_score = Resampled_Valudation_Score(X_new, y,
                                                                                            sampler=self.sampler,
                                                                                            estimator=self.estimator,
-                                                                                           average=self.average
+                                                                                           average=self.average,
                                                                                            n_splits=self.cv,
                                                                                            verbose=self.verbose)
                 IM_score = IM_score.sort_values(
